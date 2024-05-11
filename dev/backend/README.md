994732d7eaaf3f8dc09cb26dcb7ab060d43a425b
imandresi <contact@mandresi.net>
2024-05-11 20:34:34 +0300

    (docs:backend) add documentation for backend

---
+++ dev/backend/README.md
@@ -0,0 +1,8 @@
+# OVH Mail Manager - Backend
+
+To ensure the backend works properly, it's essential to configure your web server to point to its folder. You have
+various options for local web servers, including WampServer, XAMPP, and more. Choose the one that best suits your needs
+and configure and run it accordingly.[README.md](..%2F..%2FREADME.md)
+
+Next, create the `config.php` file based on the `config-sample.php`. For more detailed information, refer to the
+main documentation.
--- dev/backend/config-sample.php
+++ dev/backend/config-sample.php
@@ -17,17 +17,39 @@
  *
  */
 
+/**
+ * Put your OVH API Keys here
+ */
 define( 'OVH_APPLICATION_KEY', '' );
 define( 'OVH_APPLICATION_SECRET', '' );
 define( 'OVH_CONSUMER_KEY', '' );
 define( 'OVH_API_ENDPOINT', 'ovh-eu' );
 
+/**
+ * URL to the backend
+ * Do not change it if you use the installation from the downloaded release
+ */
+
 define( 'APP_API_ENDPOINT', '/api' );
 
+/**
+ * Just put what you want.
+ * It is used to encrypt your authorization token when for frontend/backend communications
+ */
 define( 'AUTH_SECRET_KEY', '' );
 
+/**
+ * Used to authenticate you on login form
+ */
 define( 'LOGIN_USERNAME', '' );
 define( 'LOGIN_PASSWORD', '' );
 
+/**
+ * The domain name associated to the MX Plan Hosting
+ */
 define( 'DOMAIN_NAME', '' );
+
+/**
+ * A list of email accounts you want to be excluded from listing
+ */
 define( 'EXCLUDE_ACCOUNTS', array() );
